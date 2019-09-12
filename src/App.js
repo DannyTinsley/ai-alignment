100defac98fefb949f584f74bff90fa71d50bf3b
Sashie <danielletinsley94@gmail.com>
2019-09-12 18:50:40 -0400

    minor styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React, { Component } from "react";
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Home from "./home";
+import Bills from "./bills";
+import Contact from "./contact";
+ 
+class App extends Component {
+  render() {
+    return (
+      <HashRouter>
+         <div>
+          <h1>Alignment Assignment</h1>
+          <ul className="header">
+            <li><NavLink exact to="/">Home</NavLink></li>
+            <li><NavLink to="/bills">Senate Bills</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
+          </ul>
+          <div className="content">
+            <Route exact path="/" component={Home}/>
+            <Route path="/bills" component={Bills}/>
+            <Route path="/contact" component={Contact}/>
+          </div>
+        </div>
+        </HashRouter>
+    );
+  }
 }
-
+ 
 export default App;
---
+++ src/contact.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+ 
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h2>GOT QUESTIONS?</h2>
+        <p>The easiest thing to do is check out the Advocacy Institute's  
+        <a href="http://hi.advocacy-institute.org/"> website</a>.
+        </p>
+      </div>
+    );
+  }
+}
+ 
+export default Contact;
